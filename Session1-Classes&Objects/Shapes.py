cdc22adcca6ef31250644815d8ecc50e56fc64d3
AWherzog <Herzogwalterabraham@gmail.com>
2025-09-17 11:37:54 +0200

    shapes

--- Session1-Classes&Objects/Shapes.py
+++ Session1-Classes&Objects/Shapes.py
@@ -1,3 +1,4 @@
+from math import pi
 
 class Shape:
 
@@ -9,8 +10,42 @@ class Shape:
     
     def area(self):
         raise NotImplementedError("cannot computer area")
-        
 
-s = Shape("generic shape")
-print(s.name)
-print(s.area)
+class Square(Shape):
+    def __init__(self, name, side):
+        super().__init__(name)
+        self.side = side
+
+    def perimeter(self):
+        return self.side * 4
+
+    def area(self):
+        return self.side * self.side
+
+class Circle(Shape):
+    def __init__(self, name, radius):
+        super().__init__(name)
+        self.radius = radius
+
+    def perimeter(self):
+        return 2 * pi * self.radius
+
+    def area(self):
+        return pi * self.radius * self.radius
+    
+
+l = [Square("generic square", 10), Square("another square", 5), Circle("a circle", 3)]
+
+for element in l:
+    n = element.name
+    p = element.perimeter()
+    a = element.area()
+    print(f"{n} has area {a} and perimeter {p}")
+
+s = Square("generic square", 10)
+print(s.perimeter())
+print(s.area())
+
+c = Circle("circle", 3)
+print(c.perimeter())
+print(c.area())
