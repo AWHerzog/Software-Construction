8a45d8e36aecfe7ba36f9159c7302063578a5014
AWherzog <Herzogwalterabraham@gmail.com>
2025-09-24 10:29:15 +0200

    math

--- Session2/math.py
+++ Session2/math.py
@@ -1,2 +1,22 @@
 
-def add(n1)
+def add(n1, n2):
+    result = n1 + n2
+    return result
+
+
+def my_abs(n1):
+    if n1 > 0:
+        return n1
+    if n1 < 0:
+        return -n1
+
+
+expected_result = 3
+addition_result = add(1, 2)
+if addition_result != expected_result:
+    print("Addition result is incorrect!")
+
+expected_result = 0
+absolute_result = my_abs(0)
+if absolute_result != expected_result:
+    print(f"Absolute result is incorrect! Expected {expected_result}, got {absolute_result}")
